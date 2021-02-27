cc29ef9c1659847ba0043f074cf0610e1472f92e
Yuki-At <yuki.yuki.yoh@gmail.com>
2021-02-27 09:58:18 +0900

    first commit.

---
+++ src/main/java/net/kunmc/lab/nodamageplugin/NoDamagePlugin.java
@@ -0,0 +1,14 @@
+package net.kunmc.lab.nodamageplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NoDamagePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
